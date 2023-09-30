454006b9961b6150cf56a3ea9715c20d987c022f
Abdul Basit <bbasitkdfbgvb@gmail.com>
2023-09-30 20:22:08 +0500

    Assignment Question No 1 To Question 45

--- dist/index.js
+++ dist/index.js
@@ -1,125 +1,90 @@
+"use strict";
 // My name is Abdul Basit
 //Date : 29/07/2023
 //Question no 1 ;
 //Install Node.js, TypeScript and VS Code on your computer.
-
+Object.defineProperty(exports, "__esModule", { value: true });
 //Question no 2 ;
 //Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-
 let firstName = "Abdul Basit";
 let lastName = "Abdul Dayyan";
 console.log(`${firstName} ${lastName}`);
-
 //Question no 3 ;
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
 let name1 = "Abdul Basit";
 console.log(name1.toLowerCase());
 console.log(name1.toUpperCase());
-console.log(name1.charAt(0).toUpperCase()+name1.slice(1).toLowerCase());
-
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
 //Question no 4 ;
 //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-
-let quote :string = "Failure is a word unknown to me.";
-let author :string ="Quaid-e-Azam Muhammad Ali Jinnah.";
+let quote = "Failure is a word unknown to me.";
+let author = "Quaid-e-Azam Muhammad Ali Jinnah.";
 console.log(`${quote} - ${author}`);
-
 //Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-
 //Question no 5 ;
 //Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-
 let famousPerson = "Allama Iqbal";
-let quote1 ="khod ko kar buland itna ka har taqdeer se pehle khuda bande se khud pooche bata teri raza kya hai .";
-
+let quote1 = "khod ko kar buland itna ka har taqdeer se pehle khuda bande se khud pooche bata teri raza kya hai .";
 console.log(`"${famousPerson}" : ${quote1}`);
-
 //Question no 6 ;
 //Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
-
-let nameWithWhitespace ="tab\n Abdul Basit \n\tab";
+let nameWithWhitespace = "tab\n Abdul Basit \n\tab";
 console.log(`name with whitespace: "${nameWithWhitespace}"`);
 let strippedName = nameWithWhitespace.trim();
 console.log(`stripped name : "${strippedName}"`);
-
 //Question no 7 ;
 //Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
-
 //Addition : 5 + 3 = 8
 //subtraction : 10 - 2 =8
 //Multiplication : 2 * 4 = 8
 //Division : 16 / 2 = 8 
-
 console.log(5 + 3);
 console.log(10 - 2);
 console.log(2 * 4);
 console.log(16 / 2);
-
 //Question no 8 ;
 // same Question no 7 ;
-
 //Question no 9 ;
 //Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-
-let favNumber = 7 ;
+let favNumber = 7;
 console.log("My favorite number is" + favNumber + ".");
-
 //Question no 10 ;
 //Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
 //Answer Question No 10 ;
 //This program converts temperature values between Celsius and Fahrenheit;
-
 //Question no 11 ;
 //Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-
-let names: string[] = ["Abdul Basit", "Abdul Dayyan", "Qasim", "Ahmad"];
-
+let names = ["Abdul Basit", "Abdul Dayyan", "Qasim", "Ahmad"];
 for (let i = 0; i < names.length; i++) {
     console.log(names[i]);
 }
 //Questions no 12 ;
 //Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
-
-let names1: string[] = ["Abdul Basit", "Abdul Dayyan", "Ahmad", "Qasim"];
+let names1 = ["Abdul Basit", "Abdul Dayyan", "Ahmad", "Qasim"];
 let greeting = "Hello, ";
-
 for (let i = 0; i < names1.length; i++) {
     console.log(greeting + names[i] + "!");
 }
 //Questions no 13 ;
 //Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-
 // List of favorite modes of transportation
-let transportationModes: string[] = ["car", "motorcycle", "bicycle", "helicopter", "skateboard"];
-
+let transportationModes = ["car", "motorcycle", "bicycle", "helicopter", "skateboard"];
 // Loop through the array and print statements
 for (let i = 0; i < transportationModes.length; i++) {
     let mode = transportationModes[i];
     let article = mode === "motorcycle" ? "a" : "an"; // Use "a" or "an" depending on the mode
-    
     console.log(`I would like to own ${article} ${mode}.`);
 }
 //Question no 14 ;
 //Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-
-let guests:string[]=["Abdul Basit ","Abdul Dayyan ","Qasim","Ahmad","Abbas",]
-for(let i = 0 ; i < guests.length; i++){
-    console.log(`you are invited ,${guests[i]}!`)
+let guests = ["Abdul Basit ", "Abdul Dayyan ", "Qasim", "Ahmad", "Abbas",];
+for (let i = 0; i < guests.length; i++) {
+    console.log(`you are invited ,${guests[i]}!`);
 }
 //Question no 15 ;
 //Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
-
-let guests1:string[]=["Abdul Basit ","Abdul Dayyan ","Qasim","Ahmad","Abbas",]
+let guests1 = ["Abdul Basit ", "Abdul Dayyan ", "Qasim", "Ahmad", "Abbas",];
 guests1.push("Ali");
-for(let i = 0 ; i < guests1.length; i++){
-    console.log(`you are invited ,${guests1[i]}!`)
-
+for (let i = 0; i < guests1.length; i++) {
+    console.log(`you are invited ,${guests1[i]}!`);
 }
-//Question no 15 pate 2 ;
-//• Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
-
-
-
-
-
--- index.js
+++ index.js
@@ -5,49 +5,64 @@
 //Install Node.js, TypeScript and VS Code on your computer.
 //Question no 2 ;
 //Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-let firstName = "Abdul Basit";
-let lastName = "Abdul Dayyan";
-console.log(`${firstName} ${lastName}`);
+//let firstName = "Abdul Basit";
+//let lastName = "Abdul Dayyan";
+//console.log(`${firstName} ${lastName}`);
 //Question no 3 ;
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-let name1 = "Abdul Basit";
-console.log(name1.toLowerCase());
-console.log(name1.toUpperCase());
-console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
+//let name1 = "Abdul Basit";
+//console.log(name1.toLowerCase());
+//console.log(name1.toUpperCase());
+//console.log(name1.charAt(0).toUpperCase()+name1.slice(1).toLowerCase());
 //Question no 4 ;
 //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-let quote = "Failure is a word unknown to me.";
-let author = "Quaid-e-Azam Muhammad Ali Jinnah.";
-console.log(`${quote} - ${author}`);
+//let quote :string = "Failure is a word unknown to me.";
+//let author :string ="Quaid-e-Azam Muhammad Ali Jinnah.";
+//console.log(`${quote} - ${author}`);
 //Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 //Question no 5 ;
 //Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-let famousPerson = "Allama Iqbal";
-let quote1 = "khudi ko kar buland itna ka har taqdeer se pehle khuda bande se khud pooche bata teri raza kya hai .";
-console.log(`"${famousPerson}" : ${quote1}`);
+//let famousPerson = "Allama Iqbal";
+//let quote1 ="khod ko kar buland itna ka har taqdeer se pehle khuda bande se khud pooche bata teri raza kya hai .";
+//console.log(`"${famousPerson}" : ${quote1}`);
 //Question no 6 ;
 //Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
-let nameWithWhitespace = "tab\n Abdul Basit \n\tab";
-console.log(`name with whitespace: "${nameWithWhitespace}"`);
-let strippedName = nameWithWhitespace.trim();
-console.log(`stripped name : "${strippedName}"`);
+//let nameWithWhitespace ="tab\n Abdul Basit \n\tab";
+//console.log(`name with whitespace: "${nameWithWhitespace}"`);
+//let strippedName = nameWithWhitespace.trim();
+//console.log(`stripped name : "${strippedName}"`);
 //Question no 7 ;
 //Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
 //Addition : 5 + 3 = 8
 //subtraction : 10 - 2 =8
 //Multiplication : 2 * 4 = 8
 //Division : 16 / 2 = 8 
-console.log(5 + 3);
-console.log(10 - 2);
-console.log(2 * 4);
-console.log(16 / 2);
+//console.log(5 + 3);
+//console.log(10 - 2);
+//console.log(2 * 4);
+//console.log(16 / 2);
 //Question no 8 ;
 // same Question no 7 ;
 //Question no 9 ;
 //Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-let favNumber = 7;
-console.log("My favorite number is" + favNumber + ".");
+//let favNumber = 7 ;
+//console.log("My favorite number is" + favNumber + ".");
 //Question no 10 ;
 //Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
 //Answer Question No 10 ;
-//This program converts temperature values between Celsius and Fahrenheit.
+//This program converts temperature values between Celsius and Fahrenheit;
+//Question no 11 ;
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names = ["Abdul Basit", "Abdul Dayyan", "Qasim", "Ahmad"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+//Questions no 12 ;
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names1 = ["Abdul Basit", "Abdul Dayyan", "Ahmad", "Qasim"];
+let greeting = "Hello, ";
+for (let i = 0; i < names1.length; i++) {
+    console.log(greeting + names[i] + "!");
+}
+//Questions no 13 ;
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
